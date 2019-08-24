e596fae90f0d1bb8b76871886ea84cea410dfd57
James Herbstritt <james.herbstritt@gmail.com>
2019-08-24 01:23:01 -0400

    issue-10: Complete POC for websockets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,44 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
 	title = "client";
+	url = "ws://localhost:8080";
+	websocket: WebSocket;
+	message = "Hey";
+
+	constructor() {
+		this.onClose = this.onClose.bind(this);
+		this.onError = this.onError.bind(this);
+		this.onClose = this.onMessage.bind(this);
+		this.onOpen = this.onOpen.bind(this);
+	}
+
+	hello(): void {
+		this.websocket.send(this.title);
+	}
+
+	open(): void {
+		this.websocket = new WebSocket(this.url);
+		this.websocket.onclose = this.onClose;
+		this.websocket.onerror = this.onError;
+		this.websocket.onmessage = this.onMessage;
+		this.websocket.onopen = this.onOpen;
+	}
+
+	onClose(event): void {
+		console.log("On close");
+	}
+
+	onError(event): void {
+		console.log("On error", event);
+	}
+
+	onMessage(event): void {
+		console.log("On message", event);
+		this.message = event.data;
+		console.log(event.data);
+		console.log(this.message);
+	}
+	onOpen(): void {
+		console.log("On open");
+	}
 }
