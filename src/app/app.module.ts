7e6da8680424d6b7783d736c53d5b6a344355427
James Herbstritt <james.herbstritt@gmail.com>
2019-09-20 23:23:16 -0400

    Set up basic 4 way video view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { VideoComponent } from "./components/video/video.component";
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, VideoComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
