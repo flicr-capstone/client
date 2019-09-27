c48b3eded3e01e733af6b3f0bbe8e8178fe2456a
Michael Goodnow <mmgoodnow@gmail.com>
2019-09-26 20:29:34 -0400

    Fix font awesome problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { FormsModule } from "@angular/forms";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./components/app/app.component";
 import { HelloComponent } from "./components/hello/hello.component";
 
 @NgModule({
 	declarations: [HelloComponent, AppComponent],
-	imports: [BrowserModule, AppRoutingModule, FormsModule],
+	imports: [BrowserModule, AppRoutingModule, FormsModule, FontAwesomeModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
