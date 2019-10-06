de030239c2c0c0e570d9fb69212ab80d02711964
James Herbstritt <james.herbstritt@gmail.com>
2019-10-06 19:16:59 -0400

    issue-24: Create intermediate debug ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { DebugWindowComponent } from "./components/debug-window/debug-window.component";
 import { VideoComponent } from "./components/video/video.component";
 
 @NgModule({
-	declarations: [AppComponent, VideoComponent],
+	declarations: [AppComponent, VideoComponent, DebugWindowComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],
---
+++ src/app/components/debug-window/debug-window.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+	selector: "app-debug-window",
+	templateUrl: "./debug-window.component.html",
+	styleUrls: ["./debug-window.component.scss"],
+})
+export class DebugWindowComponent implements OnInit {
+	constructor() {}
+
+	ngOnInit() {}
+}
