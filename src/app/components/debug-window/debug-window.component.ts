5c63b4b9798208908acfb76fb4f58bfb503fbd70
Michael Goodnow <mmgoodnow@gmail.com>
2020-03-06 17:13:53 -0500

    Observe key events

--- src/app/components/debug-window/debug-window.component.ts
+++ src/app/components/debug-window/debug-window.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from "@angular/core";
+import { fromEvent, Observable } from "rxjs";
+import { merge } from "rxjs";
+import {
+	distinctUntilChanged,
+	filter,
+	groupBy,
+	map,
+	mergeAll,
+} from "rxjs/operators";
+import { handleKeyDown, handleKeyUp } from "../../lib/key-events";
 
 @Component({
 	selector: "app-debug-window",
@@ -8,5 +18,20 @@ import { Component, OnInit } from "@angular/core";
 export class DebugWindowComponent implements OnInit {
 	constructor() {}
 
-	ngOnInit() {}
+	ngOnInit() {
+		const keyDowns: Observable<KeyboardEvent> = fromEvent<KeyboardEvent>(
+			document,
+			"keydown"
+		);
+		const keyUps: Observable<KeyboardEvent> = fromEvent<KeyboardEvent>(
+			document,
+			"keyup"
+		);
+
+		const keyPresses = merge(keyDowns, keyUps)
+			.pipe(filter(event => !event.repeat))
+			.subscribe(e => {
+				console.log(e);
+			});
+	}
 }
---
+++ src/app/lib/key-events.ts
@@ -0,0 +1,6 @@
+export function handleKeyDown(event: KeyboardEvent) {
+	console.log(event);
+}
+export function handleKeyUp(event: KeyboardEvent) {
+	console.log(event);
+}
